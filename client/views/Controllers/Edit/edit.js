753a802da1f01c7e0c87b1728d6848d34315b659
Daniel Clayson <danclayson@gmail.com>
2016-01-19 22:30:25 -0800

    Began working on allowing config.js download from edit. NOT FINISHED

--- client/views/Controllers/Edit/edit.js
+++ client/views/Controllers/Edit/edit.js
@@ -8,6 +8,14 @@ Template.editController.onCreated(function(){
 Template.editController.helpers({
   controller: function(){
     return Meteor.users.findOne({_id: FlowRouter.getParam('controller_id'), type: "controller", owner_id: Meteor.userId()}, {sort: {name: 1}})
+  },
+  canDownload: function(){
+    var secret = $("#secret").val();
+    if(secret){
+      return secret.length >= 10 ? "btn-warning" : "btn-default disabled";
+    } else {
+      return "btn-default disabled";
+    }
   }
 })
 
@@ -34,5 +42,25 @@ Template.editController.events({
     } else {
       bootbox.alert("Please fill out all of the fields", function(){});
     }
+  },
+  "click #downloadConfig": function(e){ //Download a config file, ready to be plopped onto the Raspberry Pi. Only works if a new secret is typed in.
+    Meteor.call('updateController', controller_id, name, desc, secret, function(error, result){
+      if(result){
+        var controller = Meteor.users.findOne({_id: FlowRouter.getParam('controller_id')})
+        var ID = controller.username,
+            secret = $("#secret").val();
+
+        var blob = new Blob([
+          "var config = {\n" +
+          "ddpHost: '" + Meteor.absoluteUrl() + "',\n" +
+          "ddpPort: 80,\n" +
+          "ddpUsername: '" + ID + "',\n" +
+          "ddpPassword:'" + secret + "'\n" +
+          "};\n\n" +
+
+          "module.exports = config;" ], {type: "text/plain;charset=utf-8"});
+        saveAs(blob, "config.js");
+      }
+    })
   }
 })
